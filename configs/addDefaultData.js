1e0b17964eef31be7ebdafb12a7d059a72852b7b
infived3 <infived3@gmail.com>
2025-05-29 11:13:08 +0530

    modified:   configs/addDefaultData.js

--- configs/addDefaultData.js
+++ configs/addDefaultData.js
@@ -1,5 +1,6 @@
 import Migration from "../models/Migration.js";
 import Option from "../models/Options.js";
+import User from "../models/User.js";
 import logger from "../utils/loggerUtil.js";
 
 const optionsData = [
@@ -7,6 +8,15 @@ const optionsData = [
   { name: "TAX SHOW", value: 1 },
   { name: "SERVICE CHARGE", value: 1 },
   { name: "SERVICE CHARGE SHOW", value: 1 },
+];
+
+const defaultUser = [
+  {
+    name: "Admin",
+    email: "admin@gmail.com",
+    password: "$2b$10$zdBeDkUF4sLL.eCL1pHYe.ObFfOLhfcuC8T6vBMvOkY4a0cLVYQVm",
+    role: "admin",
+  },
 ];
 
 export const addDefaultDBData = async () => {
@@ -24,6 +34,21 @@ export const addDefaultDBData = async () => {
         name: "Options initial migrations.",
       });
     }
+
+    const tbl2 = await Migration.findOne({
+      where: { tableName: User.tableName },
+    });
+    if (!tbl2) {
+      for (let i = 0; i < defaultUser.length; i++) {
+        let opt = defaultUser[i];
+        await User.create(opt);
+      }
+      await Migration.create({
+        tableName: User.tableName,
+        name: "User initial migrations.",
+      });
+    }
+
     logger.info("Migrations success..");
   } catch (error) {
     logger.error(error);
